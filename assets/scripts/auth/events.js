69bd24147b90fe7d1e0cabb30c7773cd10ecd877
andreasantarl <andrea.santarl@gmail.com>
2016-06-02 10:46:40 -0400

    X values can win
    If array values for winning indices are set to x, x player wins

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -9,6 +9,7 @@ let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let turn = 0;
 let move = '';
 let player = '';
+let winner = '';
 
 const onSignUp = function (event) {
   event.preventDefault();
@@ -71,6 +72,50 @@ const setTurn = function () {
     return move;
 };
 
+const checkForWin = function (gameBoard) {
+  event.preventDefault();
+  console.log('Gameboard' + gameBoard);
+  //let arrayGame = array;
+
+  if (
+      ((gameBoard[0] === gameBoard[1]) && (gameBoard[2] === gameBoard[0]) && gameBoard[0] ==='x') ||
+      ((gameBoard[3] === gameBoard[4]) && (gameBoard[5] === gameBoard[3]) && gameBoard[3] ==='x') ||
+      ((gameBoard[6] === gameBoard[7]) && (gameBoard[8] === gameBoard[6]) && gameBoard[6] ==='x') ||
+      ((gameBoard[0] === gameBoard[3]) && (gameBoard[6] === gameBoard[0]) && gameBoard[0] ==='x') ||
+      ((gameBoard[1] === gameBoard[4]) && (gameBoard[7] === gameBoard[1]) && gameBoard[1] ==='x') ||
+      ((gameBoard[2] === gameBoard[5]) && (gameBoard[8] === gameBoard[2]) && gameBoard[2] ==='x') ||
+      ((gameBoard[0] === gameBoard[4]) && (gameBoard[8] === gameBoard[0]) && gameBoard[0] ==='x') ||
+      ((gameBoard[2] === gameBoard[4]) && (gameBoard[6] === gameBoard[2]) && gameBoard[2] ==='x')) {
+        console.log(gameBoard[0]);
+          //winner = 'PlayerX';
+          console.log("player X wins!");
+    /*
+    if ((gameBoard[0] === gameBoard[1]) && (gameBoard[1] === gameBoard[2]) && (gameBoard[0] ==='x')) {
+      console.log(gameBoard[0]);
+        //winner = 'PlayerX';
+        console.log("player X wins!");
+      } else if ((gameBoard[0] === gameBoard[1]) && (gameBoard[1] === gameBoard[2]) && (gameBoard[0] ==='o')) {
+        console.log(gameBoard[0]);
+          //winner = 'PlayerX';
+          console.log("player o wins!");
+        }
+      */
+    } else if (((gameBoard[0] === gameBoard[1] === gameBoard[2]) && gameBoard[0] ==='o') ||
+      ((gameBoard[3] === gameBoard[4] === gameBoard[5]) && gameBoard[3] ==='o') ||
+      ((gameBoard[6] === gameBoard[7] === gameBoard[8]) && gameBoard[6] ==='o') ||
+      ((gameBoard[0] === gameBoard[3] === gameBoard[6]) && gameBoard[0] ==='o') ||
+      ((gameBoard[1] === gameBoard[4] === gameBoard[7]) && gameBoard[1] ==='o') ||
+      ((gameBoard[2] === gameBoard[5] === gameBoard[8]) && gameBoard[2] ==='o') ||
+      ((gameBoard[0] === gameBoard[4] === gameBoard[8]) && gameBoard[0] ==='o') ||
+      ((gameBoard[2] === gameBoard[4] === gameBoard[6]) && gameBoard[2] ==='o')) {
+        winner = 'PlayerO';
+        console.log("player O wins!");
+      } else {
+      //  console.log ("Game over!");
+    }
+
+};
+
 
 const setGameArray = function (id, move) {
   let location = parseInt(id);
@@ -80,7 +125,8 @@ const setGameArray = function (id, move) {
   } else {
     gameBoard[location] = value;
     }
-  console.log(gameBoard);
+//  console.log(gameBoard);
+  checkForWin(gameBoard);
 };
 
 //set value of text to play move
@@ -99,22 +145,8 @@ const playerMove = function (event) {
     setGameArray(id, move);
   };
 
-/*
-  const checkForWin = function (array) {
-    event.preventDefault();
-    let game = array;
-    if (array[0] === array[1] === array[2] ||
-      array[3] === array[4] === array[5] ||
-      array[6] === array[7] === array[8] ||
-      array[0] === array[3] === array[6] ||
-      array[1] === array[4] === array[7] ||
-      array[2] === array[5] === array[8] ||
-      array[0] === array[4] === array[8] ||
-      array[2] === array[4] === array[6]) {
-        if
-      }
-  }
-  */
+
+
 
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp);
@@ -135,7 +167,7 @@ const addHandlers = () => {
 };
 
 $(document).ready(function () {
-  console.log(gameBoard);
+//  console.log(gameBoard);
 });
 
 module.exports = {
